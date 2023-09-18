3eac05b72669d9dca6170f6316bd2ffee8a42367
Bhooker88 <139884664+Bhooker88@users.noreply.github.com>
2023-09-18 21:29:05 +0000

    final list

--- src/js/component/App.jsx
+++ src/js/component/App.jsx
@@ -1,40 +1,95 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const App = () =>{
-    const [list, setList] = useState ([]);
-    const [input, setInput] = useState ("");
-
-    const addTodo = (todo) => {
-        const newTodo= {
-            id: Math.random(),
-            todo: todo
-        };
-
-        setList([...list, newTodo]);
-        setInput("");
-
-    };
-
-    const deletetodo = (id) =>{
-        const newList = list.filter((todo) => todo.id !== id);
-        setList(newList);
-    }
-
+function Todo({ todo, index, completeTodo, removeTodo }) {
     return (
-        <div className="todoList bg-light py-5 ps-5 pe-5 mt-5">
-            <h1>Todo List</h1>
-            <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
-            <button onClick={() => addTodo(input)}>Add</button>
-            <ul>
-               {list.map((todo) =>(
-                <li key={todo.id}>
-                    {todo.todo}
-                    <button className="ms-2" onClick={() => deletetodo(todo.id)}>&times;</button>
-                </li>
-               ))}
-            </ul>
+      <div
+        className="todo"
+        style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+      >
+        {todo.text}
+        <div>
+          <button onClick={() => completeTodo(index)}>Complete</button>
+          <button onClick={() => removeTodo(index)}>x</button>
         </div>
-    )
-}
-
-export default App; 
+      </div>
+    );
+  }
+  
+  function TodoForm({ addTodo }) {
+    const [value, setValue] = React.useState("");
+  
+    const handleSubmit = e => {
+      e.preventDefault();
+      if (!value) return;
+      addTodo(value);
+      setValue("");
+    };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="input"
+          placeholder='Add todo'
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
+      </form>
+    );
+  }
+  
+  function App() {
+    const [todos, setTodos] = React.useState([
+      {
+        text: "Walk the dog",
+        isCompleted: false
+      },
+      {
+        text: "Do laundry",
+        isCompleted: false
+      },
+      {
+        text: "Go to the grocery store",
+        isCompleted: false
+      }
+    ]);
+  
+    const addTodo = text => {
+      const newTodos = [...todos, { text }];
+      setTodos(newTodos);
+    };
+  
+    const completeTodo = index => {
+      const newTodos = [...todos];
+      newTodos[index].isCompleted = true;
+      setTodos(newTodos);
+    };
+  
+    const removeTodo = index => {
+      const newTodos = [...todos];
+      newTodos.splice(index, 1);
+      setTodos(newTodos);
+    };
+  
+    return (
+      <div className="todoList my-5 ms-5 me-5 py-5 ps-5 pe-5">
+        <h1>Todo List</h1>
+        <div className="d-flex">
+         <TodoForm addTodo={addTodo}/>
+         </div>
+        <div>
+          {todos.map((todo, index) => (
+            <Todo
+              key={index}
+              index={index}
+              todo={todo}
+              completeTodo={completeTodo}
+              removeTodo={removeTodo}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+  
+  export default App;
